3079d96786373e6bc3e5221b1bb545263926b4d7
dingxin <zzuxind@163.com>
2019-08-23 18:45:23 +0800

    modify

--- src/main/java/com/thoughtworks/tdd/Game.java
+++ src/main/java/com/thoughtworks/tdd/Game.java
@@ -1,13 +1,28 @@
 package com.thoughtworks.tdd;
 
 public class Game {
-	private static String Fizz="fizz";
+	private static String FIZZ="Fizz";
+	private static String BUZZ="Buzz";
+	private static String WHIZZ="Whizz";
 
 	public String play(int number) {
 		String resultString="";
-		if(number == 1) {
-			resultString="1";
+		if(number<1 || number>105) {
+			resultString="invalid";
+		}else {
+			if(number % 3 == 0) {
+				resultString+=FIZZ;
+			}
+			if(number % 5 ==0) {
+				resultString+=BUZZ;
+			}
+			if(number % 7 == 0) {
+				resultString+=WHIZZ;
+			}if(resultString == "") {
+				resultString=String.valueOf(number);
+			}
 		}
+		
 	
 		return resultString;
 }
--- src/test/java/com/thoughtworks/tdd/GameTest.java
+++ src/test/java/com/thoughtworks/tdd/GameTest.java
@@ -18,6 +18,44 @@ public class GameTest {
 		Assertions.assertEquals("Fizz", gameString);
 	}
 
+	@Test
+	public void should_return_Buzz_when_input_5() {
+		Game game=new Game();
+		String gameString=game.play(5);
+		Assertions.assertEquals("Buzz", gameString);
+	}
+	
+	@Test
+	public void should_return_Whizz_when_input_7() {
+		Game game=new Game();
+		String gameString=game.play(7);
+		Assertions.assertEquals("Whizz", gameString);
+	}
+	
+	@Test
+	public void should_return_FizzBuzz_when_input_15() {
+		Game game=new Game();
+		String gameString=game.play(15);
+		Assertions.assertEquals("FizzBuzz", gameString);
+	}
+	@Test
+	public void should_return_BuzzWhizz_when_input_35() {
+		Game game=new Game();
+		String gameString=game.play(35);
+		Assertions.assertEquals("BuzzWhizz", gameString);
+	}
+	@Test
+	public void should_return_FizzWhizz_when_input_21() {
+		Game game=new Game();
+		String gameString=game.play(21);
+		Assertions.assertEquals("FizzWhizz", gameString);
+	}
+	@Test
+	public void should_return_FizzBuzzWhizz_when_input_21() {
+		Game game=new Game();
+		String gameString=game.play(105);
+		Assertions.assertEquals("FizzBuzzWhizz", gameString);
+	}
 	
 
 }
